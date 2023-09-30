ce4ef17a7b8490374ee86c4b230d55e9e7c5f753
Stephanie Strano <stranosteph@gmail.com>
2023-09-30 13:37:40 -0400

    Tested shopping cart by adding my list of books to read for autumn 2023

--- src/main/java/com/bookstore/Bookstore.java
+++ src/main/java/com/bookstore/Bookstore.java
@@ -6,7 +6,20 @@ public class Bookstore {
         System.out.println("Welcome to Books & Beans!");
 
         Book ifWeWereVillains = new Book("If We Were Villains", "M.L. Rio", 17.99);
-        System.out.println(ifWeWereVillains.bookDetails());
+        Book theOnceAndFutureWitches = new Book("The Once and Future Witches", "Alix E. Harrow", 17.99);
+        Book suchSharpTeeth = new Book("Such Sharp Teeth", "Rachel Harrison", 17.00);
+        Book theMaidens = new Book("The Maidens", "Alex Michaelides", 17.99);
+        Book youAgain = new Book("You Again", "Kate Goldbeck", 24.99);
+
+        ShoppingCart autumnBooks = new ShoppingCart();
+
+        autumnBooks.add(ifWeWereVillains);
+        autumnBooks.add(theOnceAndFutureWitches);
+        autumnBooks.add(suchSharpTeeth);
+        autumnBooks.add(theMaidens);
+        autumnBooks.add(youAgain);
+
+        System.out.println(autumnBooks.receipt());
     }
 
 }
