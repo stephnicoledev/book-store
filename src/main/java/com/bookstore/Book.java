55eeaa670ab5f9992ea868f4a9b9496a3213ea07
Stephanie Strano <stranosteph@gmail.com>
2023-09-30 13:17:40 -0400

    Added constructor and toString() method to complete the Book class

--- src/main/java/com/bookstore/Book.java
+++ src/main/java/com/bookstore/Book.java
@@ -5,6 +5,12 @@ public class Book {
     private String author;
     private double price;
 
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -27,6 +33,15 @@ public class Book {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
 
