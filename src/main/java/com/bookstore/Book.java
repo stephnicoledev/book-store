3cec0adbccdb12cf93170c0ea990a1d5fc8385e5
Stephanie Strano <stranosteph@gmail.com>
2023-09-30 13:14:43 -0400

    Created instance variables for the Book class

---
+++ src/main/java/com/bookstore/Book.java
@@ -0,0 +1,7 @@
+package com.bookstore;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+}
---
+++ src/main/java/com/bookstore/Bookstore.java
@@ -0,0 +1,9 @@
+package com.bookstore;
+
+public class Bookstore {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Books & Beans!");
+    }
+
+}
